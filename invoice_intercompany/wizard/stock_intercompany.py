c094e29a3d4283f3d29f409bf756226389726b4b
CaparrosC <ccaparros@yziact.fr>
2021-09-21 16:51:29 +0200

    [FIX] intercompany

--- invoice_intercompany/wizard/stock_intercompany.py
+++ invoice_intercompany/wizard/stock_intercompany.py
@@ -7,64 +7,68 @@ from odoo.exceptions import UserError
 class StockIntercompany(models.TransientModel):
     _name = 'stock.intercompany'
 
-    source_company_id = fields.Many2one(string="Source company", comodel_name="res.company", required=True)
-    destination_company_id = fields.Many2one(string="Destination company", comodel_name="res.company", required=True)
-    source_location_id = fields.Many2one(string="Destination location", comodel_name="stock.location", required=True)
-    destination_location_id = fields.Many2one(string="Destination location", comodel_name="stock.location", required=True)
-    origin = fields.Char(string="Origin", required=True)
-    line_ids = fields.One2many(string="Products to transfer", comodel_name="stock.intercompany.line", inverse_name="intercompany_id", required=True)
+    #### RELATION ####
+    source_company_id = fields.Many2one(string="Société source", comodel_name="res.company", required=True)
+    destination_company_id = fields.Many2one(string="Société de destination", comodel_name="res.company", required=True)
+    source_location_id = fields.Many2one(string="Emplacement source", comodel_name="stock.location", required=True, domain="[('company_id', '=', source_company_id)]")
+    destination_location_id = fields.Many2one(string="Emplacement de destination", comodel_name="stock.location", required=True, domain="[('company_id', '=', destination_company_id)]")
+    line_ids = fields.One2many(string="Articles à transférer", comodel_name="stock.intercompany.line", inverse_name="stock_intercompany_id")
+
+    #### TEXT ####
+    origin = fields.Char(string="Origine", required=True)
 
     def _check_data(self):
         """
-        check data given by the user
+        check data of the wizard
         :return:
         """
         if len(self.line_ids) == 0:
-            raise UserError("You must enter at least one line")
+            raise UserError("Vous devez saisir au moins une ligne")
         elif self.source_company_id == self.destination_company_id:
-            raise UserError("Companies must be different")
+            raise UserError("Les sociétés doivent être différentes")
         elif not self.source_location_id.company_id or not self.destination_location_id.company_id:
-            raise UserError("Locations are incorrect")
+            raise UserError("Les emplacements ne sont pas tous liés à une société")
         elif self.source_location_id.company_id == self.destination_location_id.company_id:
-            raise UserError("Locations can't have the same company")
+            raise UserError("Les emplacements doivent appartenir à deux sociétés différentes")
 
         if not self.origin:
-            self.update({'origin': "Intercompany"})
+            self.update({'origin': "Intersociété"})
 
-    def _prepare_stock_picking_values(self, type):
+    def _prepare_stock_picking_values(self, picking_type):
         """
-        prepare values for a picking according to his type
-        :param type: delivery or receipt
+        prepare values for a picking according to its type
+        :param picking_type: delivery or receipt
         :return:
         """
         values = {'origin': self.origin}
 
-        if type == "delivery":
-            picking_type = self.env['stock.picking.type'].search([('code', '=', 'outgoing'), ('company_id', '=', self.source_company_id.id)], limit=1)
+        # the picking type determines locations, partner and company
+        if picking_type == "delivery":
+            picking_type_id = self.env['stock.picking.type'].search([('code', '=', 'outgoing'), ('company_id', '=', self.source_company_id.id)], limit=1)
 
-            if not picking_type:
-                raise UserError("Picking type can't be found")
+            if not picking_type_id:
+                raise UserError("Le type de transfert n'a pas été trouvé")
 
             values.update({
                 'partner_id': self.destination_company_id.partner_id.id,
-                'company_id': self.source_company_id,
-                'location_id': self.source_location_id,
-                'picking_type_id': picking_type.id
+                'company_id': self.source_company_id.id,
+                'location_id': self.source_location_id.id,
+                'picking_type_id': picking_type_id.id
             })
-        elif type == "receipt":
-            picking_type = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('company_id', '=', self.destination_company_id.id)], limit=1)
+        elif picking_type == "receipt":
+            picking_type_id = self.env['stock.picking.type'].search([('code', '=', 'incoming'), ('company_id', '=', self.destination_company_id.id)], limit=1)
 
             if not picking_type:
-                raise UserError("Picking type can't be found")
+                raise UserError("Le type de transfert n'a pas été trouvé")
 
             values.update({
                 'partner_id': self.source_company_id.partner_id.id,
-                'company_id': self.destination_company_id,
-                'location_dest_id': self.destination_location_id,
-                'picking_type_id': picking_type.id
+                'company_id': self.destination_company_id.id,
+                'location_dest_id': self.destination_location_id.id,
+                'picking_type_id': picking_type_id.id
             })
         else:
-            raise UserError("The picking can't be created")
+            raise UserError("Le type de transfert est mauvais")
 
         return values
 
@@ -77,38 +81,53 @@ class StockIntercompany(models.TransientModel):
 
         for line in self.line_ids:
             lines.append((0, False, {
-                'product_id': line.product_id,
+                'product_id': line.product_id.id,
                 'product_uom_qty': line.quantity,
-                'lot_id': line.lot_id
+                'lot_id': line.lot_id.id
             }))
 
         return lines
 
     def cancel_pickings(self, pickings):
+        """
+        cancel the given pickings
+        :param pickings:
+        :return:
+        """
         for picking in pickings:
             picking.action_cancel()
 
-        raise UserError("Something went wrong during the pickings creation, the process is canceled")
+        raise UserError("Quelque chose s'est mal passé durant la création des transferts, le processus est annulé")
 
     def update_lots(self, pickings):
+        """
+        change the state of the given pickings and modify their lots
+        :param pickings:
+        :return:
+        """
         for picking in pickings:
+            # change the state to create the stock move lines
             picking.action_confirm()
             picking.action_assign()
 
+            # check that the lines have been created
             if len(picking.move_line_ids_without_package) == 0 or \
                     sum([line.product_uom_qty for line in picking.move_line_ids_without_package]) != \
                     sum([line.quantity for line in self.line_ids]):
                 self.cancel_pickings(pickings)
 
+            # reset lots
             picking.move_line_ids_without_package.write({'lot_id': False})
 
+            # affect lots given in the wizard
             for line in self.line_ids:
                 matching_lines = picking.move_line_ids_without_package.filtered(lambda rec: rec.product_id == line.product_id and not rec.lot_id)
 
+                # if there is no matching line, something went wrong during the creation of the lines
                 if not matching_lines:
                     self.cancel_pickings(pickings)
 
-                matching_lines[0].write({'lot_id': line.lot_id})
+                matching_lines[0].write({'lot_id': line.lot_id.id})
 
     def run(self):
         """
--- invoice_intercompany/wizard/stock_intercompany_line.py
+++ invoice_intercompany/wizard/stock_intercompany_line.py
@@ -6,7 +6,16 @@ from odoo import models, fields, api
 class StockIntercompanyLine(models.TransientModel):
     _name = 'stock.intercompany.line'
 
-    product_id = fields.Many2one(string="Product", comodel_name="product.product")
-    quantity = fields.Float(string="Quantity")
-    lot_id = fields.Many2one(string="Serial number", comodel_name="stock.production.lot")
-    intercompany_id = fields.Many2one(string="Move", comodel_name="stock.intercompany")
+    def _get_lot_id_domain(self):
+        return [
+            ('product_id', '=', self.product_id.id),
+            ('company_id', '=', self.stock_intercompany_id.source_company_id.id)
+        ]
+
+    #### RELATION ####
+    product_id = fields.Many2one(string="Article", comodel_name="product.product", required=True)
+    lot_id = fields.Many2one(string="Numéro de série", comodel_name="stock.production.lot", domain=_get_lot_id_domain, required=True)
+    stock_intercompany_id = fields.Many2one(string="Transfert IS", comodel_name="stock.intercompany")
+
+    #### NUMBER ####
+    quantity = fields.Float(string="Quantité")
